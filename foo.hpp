0168e3e65ef7963c6d59385c68848e2b3aa5fd02
Szymon Wilczek <szymon.wilczek03@gmail.com>
2025-12-04 16:59:38 +0100

    usuniecie builda]

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,21 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
+    std::vector<char> wektor;
+
+    for (Human& human : people) {
+    human.birthday();
+
+    if (human.isMonster() == true) {
+        wektor.emplace_back('n');
+    }
+    else {
+        wektor.emplace_back('y');
+    }
+    
+}
+
+std::reverse(wektor.begin(), wektor.end());
     // Twoja implementacja tutaj
-    return {};
+return wektor;
 }
